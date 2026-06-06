afefe847c87c112a7cfe386f371b98f908e6f9be
Chang, Hsin-Chi <r14227123@g.ntu.edu.tw>
2026-06-06 08:56:16 +0800

    Add windowspath_Dartel_create_Templat_job.m file

---
+++ Document/windowspath_Dartel_create_Templat_job.m
@@ -0,0 +1,110 @@
+%-----------------------------------------------------------------------
+% Job saved on 06-Jun-2026 08:53:10 by cfg_util (rev $Rev: 8183 $)
+% spm SPM - SPM25 (25.01.02)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+%%
+matlabbatch{1}.spm.tools.dartel.warp.images = {
+                                               {
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc1sub-ident01_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc1sub-ident02_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc1sub-ident03_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc1sub-ident04_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc1sub-ident05_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc1sub-ident06_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc1sub-ident07_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc1sub-ident08_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc1sub-ident09_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc1sub-ident10_run-02_T1w.nii,1'
+                                               }
+                                               {
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc2sub-ident01_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc2sub-ident02_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc2sub-ident03_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc2sub-ident04_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc2sub-ident05_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc2sub-ident06_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc2sub-ident07_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc2sub-ident08_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc2sub-ident09_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc2sub-ident10_run-02_T1w.nii,1'
+                                               }
+                                               {
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc3sub-ident01_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc3sub-ident02_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc3sub-ident03_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc3sub-ident04_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc3sub-ident05_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc3sub-ident06_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc3sub-ident07_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc3sub-ident08_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc3sub-ident09_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc3sub-ident10_run-02_T1w.nii,1'
+                                               }
+                                               {
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc4sub-ident01_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc4sub-ident02_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc4sub-ident03_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc4sub-ident04_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc4sub-ident05_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc4sub-ident06_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc4sub-ident07_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc4sub-ident08_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc4sub-ident09_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc4sub-ident10_run-02_T1w.nii,1'
+                                               }
+                                               {
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc5sub-ident01_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc5sub-ident02_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc5sub-ident03_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc5sub-ident04_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc5sub-ident05_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc5sub-ident06_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc5sub-ident07_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc5sub-ident08_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc5sub-ident09_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc5sub-ident10_run-02_T1w.nii,1'
+                                               }
+                                               {
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc6sub-ident01_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc6sub-ident02_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc6sub-ident03_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc6sub-ident04_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc6sub-ident05_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc6sub-ident06_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc6sub-ident07_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc6sub-ident08_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc6sub-ident09_run-02_T1w.nii,1'
+                                               'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\rc6sub-ident10_run-02_T1w.nii,1'
+                                               }
+                                               }';
+%%
+matlabbatch{1}.spm.tools.dartel.warp.settings.template = 'Template';
+matlabbatch{1}.spm.tools.dartel.warp.settings.rform = 0;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(1).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(1).rparam = [4 2 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(1).K = 0;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(1).slam = 16;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(2).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(2).rparam = [2 1 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(2).K = 0;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(2).slam = 8;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(3).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(3).rparam = [1 0.5 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(3).K = 1;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(3).slam = 4;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(4).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(4).rparam = [0.5 0.25 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(4).K = 2;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(4).slam = 2;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(5).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(5).rparam = [0.25 0.125 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(5).K = 4;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(5).slam = 1;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(6).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(6).rparam = [0.25 0.125 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(6).K = 6;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(6).slam = 0.5;
+matlabbatch{1}.spm.tools.dartel.warp.settings.optim.lmreg = 0.01;
+matlabbatch{1}.spm.tools.dartel.warp.settings.optim.cyc = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.optim.its = 3;
