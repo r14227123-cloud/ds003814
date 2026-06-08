5f086521327f73eadca5010458136c2f98e74bca
Chang, Hsin-Chi <r14227123@g.ntu.edu.tw>
2026-06-08 14:37:14 +0800

    Add Dartel normalize MNI script

---
+++ Document/Dartel_normalize_MNI_job.m
@@ -0,0 +1,81 @@
+%-----------------------------------------------------------------------
+% Job saved on 08-Jun-2026 14:34:45 by cfg_util (rev $Rev: 8183 $)
+% spm SPM - SPM25 (25.01.02)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.tools.dartel.mni_norm.template = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\Template_6.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(1).flowfield = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\u_rc1sub-ident01_run-02_T1w_Template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(1).images = {
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident01\func\arsub-ident01_task-tomloc2_run-01_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident01\func\arsub-ident01_task-tomloc2_run-02_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident01\func\arsub-ident01_task-tomloc2_run-03_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident01\func\arsub-ident01_task-tomloc2_run-04_echo-2_bold.nii'
+                                                                };
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(2).flowfield = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\u_rc1sub-ident02_run-02_T1w_Template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(2).images = {
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident02\func\arsub-ident02_task-tomloc2_run-01_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident02\func\arsub-ident02_task-tomloc2_run-02_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident02\func\arsub-ident02_task-tomloc2_run-03_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident02\func\arsub-ident02_task-tomloc2_run-04_echo-2_bold.nii'
+                                                                };
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(3).flowfield = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\u_rc1sub-ident03_run-02_T1w_Template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(3).images = {
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident03\func\arsub-ident03_task-tomloc2_run-01_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident03\func\arsub-ident03_task-tomloc2_run-02_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident03\func\arsub-ident03_task-tomloc2_run-03_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident03\func\arsub-ident03_task-tomloc2_run-04_echo-2_bold.nii'
+                                                                };
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(4).flowfield = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\u_rc1sub-ident04_run-02_T1w_Template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(4).images = {
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident04\func\arsub-ident04_task-tomloc2_run-01_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident04\func\arsub-ident04_task-tomloc2_run-02_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident04\func\arsub-ident04_task-tomloc2_run-03_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident04\func\arsub-ident04_task-tomloc2_run-04_echo-2_bold.nii'
+                                                                };
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(5).flowfield = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\u_rc1sub-ident05_run-02_T1w_Template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(5).images = {
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident05\func\arsub-ident05_task-tomloc2_run-01_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident05\func\arsub-ident05_task-tomloc2_run-02_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident05\func\arsub-ident05_task-tomloc2_run-03_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident05\func\arsub-ident05_task-tomloc2_run-04_echo-2_bold.nii'
+                                                                };
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(6).flowfield = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\u_rc1sub-ident06_run-02_T1w_Template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(6).images = {
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident06\func\arsub-ident06_task-tomloc2_run-01_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident06\func\arsub-ident06_task-tomloc2_run-02_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident06\func\arsub-ident06_task-tomloc2_run-03_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident06\func\arsub-ident06_task-tomloc2_run-04_echo-2_bold.nii'
+                                                                };
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(7).flowfield = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\u_rc1sub-ident07_run-02_T1w_Template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(7).images = {
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident07\func\arsub-ident07_task-tomloc2_run-01_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident07\func\arsub-ident07_task-tomloc2_run-02_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident07\func\arsub-ident07_task-tomloc2_run-03_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident07\func\arsub-ident07_task-tomloc2_run-04_echo-2_bold.nii'
+                                                                };
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(8).flowfield = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\u_rc1sub-ident08_run-02_T1w_Template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(8).images = {
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident08\func\arsub-ident08_task-tomloc2_run-01_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident08\func\arsub-ident08_task-tomloc2_run-02_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident08\func\arsub-ident08_task-tomloc2_run-03_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident08\func\arsub-ident08_task-tomloc2_run-04_echo-2_bold.nii'
+                                                                };
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(9).flowfield = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\u_rc1sub-ident09_run-02_T1w_Template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(9).images = {
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident09\func\arsub-ident09_task-tomloc2_run-01_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident09\func\arsub-ident09_task-tomloc2_run-02_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident09\func\arsub-ident09_task-tomloc2_run-03_echo-2_bold.nii'
+                                                                '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident09\func\arsub-ident09_task-tomloc2_run-04_echo-2_bold.nii'
+                                                                };
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(10).flowfield = {'C:\Users\cindy\Desktop\BH\final\SST\DARTEL_RUN\u_rc1sub-ident10_run-02_T1w_Template.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subj(10).images = {
+                                                                 '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident10\func\arsub-ident10_task-tomloc2_run-01_echo-2_bold.nii'
+                                                                 '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident10\func\arsub-ident10_task-tomloc2_run-02_echo-2_bold.nii'
+                                                                 '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident10\func\arsub-ident10_task-tomloc2_run-03_echo-2_bold.nii'
+                                                                 '\\wsl$\Ubuntu\home\chsinchi\subjects_nii\ds003814\sub-ident10\func\arsub-ident10_task-tomloc2_run-04_echo-2_bold.nii'
+                                                                 };
+matlabbatch{1}.spm.tools.dartel.mni_norm.vox = [2.4 2.4 2.5];
+matlabbatch{1}.spm.tools.dartel.mni_norm.bb = [NaN NaN NaN
+                                               NaN NaN NaN];
+matlabbatch{1}.spm.tools.dartel.mni_norm.preserve = 0;
+matlabbatch{1}.spm.tools.dartel.mni_norm.fwhm = [7 7 7];
